8dac02b0345f4caa8b87546dd183b1ddff1cce14
davidyshin <davidshin@ac.c4q.nyc>
2018-03-03 17:57:50 -0500

    style fixes

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -17,24 +17,21 @@ class List extends React.Component {
 
       <div className="location-list">
         <h3 className="pharm-header"> Pharmacies </h3>
-        <div className="pharm-list-div">
-
           {data.map(pharm => {
             return (
               <ul className="pharm-list">
                 <li className="pharm-list-item" id={pharm.a}>
                   <h3>{pharm.facility_name}</h3>
                   <ul className="pharm-list-info">
-                    <li><span className="italics">Address:</span> {pharm.address + " " + pharm.borough + " " + pharm.zip_code}</li>
-                    <li><span className="italics">Contact:</span> {pharm.phone}</li>
-                    <li><span className="italics">Vaccines for kids:</span> {pharm.children}</li>
+                    <p><span className="italics">Address:</span> {pharm.address + " " + pharm.borough + " " + pharm.zip_code}</p>
+                    <p><span className="italics">Contact:</span> {pharm.phone}</p>
+                    <p><span className="italics">Vaccines for kids:</span> {pharm.children}</p>
                   </ul>
                 </li>
               </ul>
             );
           })}
         </div>
-      </div>
     );
   }
 }
