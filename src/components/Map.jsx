b9dbb95783563104296fc2e18769c14ff5afcd15
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2018-03-03 17:16:00 -0500

    added css styling in map.jsx and app.css, map is sized and list is fixed sized

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -43,7 +43,7 @@ class Map extends React.Component {
     const { data } = this.state;
     return (
       // Google Maps Container
-      <div>
+      <div className="map-page">
         <div className="map-container">
           <GoogleMapReact
             bootstrapURLKeys={{
@@ -55,6 +55,9 @@ class Map extends React.Component {
             {/*Map pins according to data here*/}
             {data.map(this.renderPin)}
           </GoogleMapReact>
+        </div>
+        <div className="location-list">
+        
         </div>
       </div>
     );
